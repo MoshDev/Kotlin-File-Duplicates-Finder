21fd7183273f7966d29cc0fe7bfe8c4c08b0c6c1
Mohammad Ersan <mr.ersan85@gmail.com>
2017-12-13 01:23:02 +0000

    refactor readableSize to use Kotlin String templates

--- src/main/java/com/ersan/space/Main.java
+++ src/main/java/com/ersan/space/Main.java
@@ -7,10 +7,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        if (args == null || args.length == 0) {
-            throw new IllegalArgumentException("Please provide the directory path to scan");
-        }
-        String inputDir = args[0];
+//        if (args == null || args.length == 0) {
+//            throw new IllegalArgumentException("Please provide the directory path to scan");
+//        }
+        String inputDir = "/Users/ersan/Android/Bitbucket/sample_test";
         Starter starter = new Starter(new Args(inputDir));
         starter.deletedDuplicates();
 
